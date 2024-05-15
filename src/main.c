f26222f175230d6ececaee81ff7df2f45d282f02
A.M.S <99796232+outsiderice@users.noreply.github.com>
2024-05-15 15:11:24 +0200

    Wroking on readline

--- src/main.c
+++ src/main.c
@@ -3,22 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: amagnell <amagnell@student.42barcel>       +#+  +:+       +#+        */
+/*   By: amagnell <amagnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 12:29:38 by amagnell          #+#    #+#             */
-/*   Updated: 2024/05/09 12:45:41 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/05/15 14:53:41 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"minishell.h"
 
+char	*ft_readline(void)
+{
+	char	*line;
+	char	*prompt;
+
+	prompt = "eggshell~$";
+	line = readline (prompt);
+	if (!line)
+		exit(EXIT_FAILURE);
+	return (line);
+}
+
 void	ft_minishell(char **env)
 {
+	(void)env;
+	char	*line;
+
+	line = NULL;
+	while (2)
+	{
+		line = ft_readline();
+		free (line);
+	}
 	return ;
 }
 
 int	main(int argc, char **argv, char **env)
 {
-	ft_minishell(char **env);
+	(void)argv;
+	if (argc != 1)
+		return (EXIT_FAILURE);
+	ft_minishell(env);
 	return (0);
 }
