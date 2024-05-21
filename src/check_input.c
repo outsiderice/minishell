55cccde1bcea13533a390c535f81366e11877382
A.M.S <99796232+outsiderice@users.noreply.github.com>
2024-05-21 11:19:32 +0200

    removed old tests

--- src/check_input.c
+++ src/check_input.c
@@ -6,7 +6,7 @@
 /*   By: amagnell <amagnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 17:43:53 by amagnell          #+#    #+#             */
-/*   Updated: 2024/05/21 11:13:58 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/05/21 11:19:12 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,18 +48,15 @@ int	ft_check_quotes(const char *line)
 	int	qt_len;
 
 	i = 0;
-	//ft_printf("i = %d\n", i); //test
 	while (line[i])
 	{
 		if (line[i] == '"' || line[i] == '\'')
 		{
 			qt_len = ft_quote_len(&line[i], line[i]);
 			i = i + qt_len;
-			//ft_printf("i is now %d\n", i); //test
 		}
 		else
 			i++;
-		//ft_printf("i is %d\n", i); //test
 	}
 	return (0);
 }
@@ -68,6 +65,5 @@ int	ft_check_input(const char *line)
 {
 	ft_check_quotes(line);
 	ft_check_pipes(line);
-	//ft_printf("out\n"); //test
 	return (EXIT_SUCCESS);
 }
