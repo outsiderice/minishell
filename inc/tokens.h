b4e3e8fe4017495da547ff726c79a2be41e7a4c7
A.M.S <99796232+outsiderice@users.noreply.github.com>
2024-05-21 09:33:08 +0200

    explained token type usage

--- inc/tokens.h
+++ inc/tokens.h
@@ -6,19 +6,21 @@
 /*   By: amagnell <amagnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 12:47:27 by amagnell          #+#    #+#             */
-/*   Updated: 2024/05/21 08:52:22 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/05/21 09:30:53 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TOKENS_H
 # define TOKENS_H
 
-typedef struct s_tokens
+//type = 1 ->word
+//type = 2 ->operator //might assign from 2-5 for redirections if needed
+typedef struct	s_tokens
 {
-    int             type;
-    char            *token;
-    struct  s_token *next;
-} t_tokens;
+	int				type;
+	char			*token;
+	struct s_token	*next;
+}					t_tokens;
 
 
 void	ft_tokenize(const char *line);
