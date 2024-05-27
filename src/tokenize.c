4c210a41074dee08cf4e73749eeb6eaa5987ac8b
A.M.S <99796232+outsiderice@users.noreply.github.com>
2024-05-27 15:40:41 +0200

    test ok

--- src/tokenize.c
+++ src/tokenize.c
@@ -6,7 +6,7 @@
 /*   By: amagnell <amagnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 12:47:08 by amagnell          #+#    #+#             */
-/*   Updated: 2024/05/27 15:32:53 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/05/27 15:40:30 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,11 +91,5 @@ void	ft_tokenize(const char *line, t_ms *ms)
 			i = i + ft_space_len(&line[i]);
 	}
 	ms->tokens = toks;
-	while (toks->next != NULL)
-	{
-		printf("token is %s, type %d\n", toks->tok, toks->type);
-		toks = toks->next;
-	}
-	printf("token is %s, type %d\n", toks->tok, toks->type);
 	//ft_parse(tokens);
 }
