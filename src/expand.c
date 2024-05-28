00ad1c1c68692b025c590c52462bb73b6afd9307
A.M.S <99796232+outsiderice@users.noreply.github.com>
2024-05-28 15:39:27 +0200

    change to redir_syntax

--- src/expand.c
+++ src/expand.c
@@ -6,21 +6,19 @@
 /*   By: amagnell <amagnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/24 13:37:06 by amagnell          #+#    #+#             */
-/*   Updated: 2024/05/28 11:41:04 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/05/28 11:58:55 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 //find the $ which must be expanded
-//look for the value and substiute for what's found in env 
+//look for the value and substitute for what's found in env 
 //or an empty string if not found
 //remove quotes
 
 //find $ signs to be expanded in the tokens
-//checks if a token is type 0-word or type 1-quoted and that the quotes are "
-//	then looks for a dollar sign inside these specific tokens
-//	when a dollar sign is found calls an expansion function
+//when a viable $ is found calls ft_expand
 void	ft_expansion_check(t_ms *ms)
 {
 	t_tokens	*current;
--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: amagnell <amagnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 13:17:12 by amagnell          #+#    #+#             */
-/*   Updated: 2024/05/27 19:10:12 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/05/28 15:37:40 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	ft_pipe_syntax(t_tokens *tok)
 //if the token next to a redirection is not a word (type 0) give an error and nl
 void	ft_redir_syntax(t_tokens *tok)
 {
-	if (tok->next == NULL || tok->next->type != 0) //expansion needs to be before this
+	if (tok->next == NULL || (tok->next->type != 0 && tok->next->type != 1))
 		printf("redir syntax error\n"); //add proper error and nl
 }
 
