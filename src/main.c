d408c9dfb0b7823e2adef2d0738ac2342f232921
outsiderice <arantxamagnelli@gmail.com>
2024-06-07 12:10:23 +0200

    reviewing for protections and clean exits

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: amagnell <amagnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 12:29:38 by amagnell          #+#    #+#             */
-/*   Updated: 2024/06/07 11:23:03 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/06/07 12:07:17 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 void	ft_init_ms(t_ms *ms)
 {
-
 	ms->env = NULL;
 	ms->tokens = NULL;
 	//ms->args = NULL;
@@ -22,9 +21,11 @@ void	ft_init_ms(t_ms *ms)
 	ms->sh_lvl = -1; //HOW?
 	ms->old_pwd = NULL;
 	ms->new_pwd = getcwd(NULL, 0);
+	//if (ms->new_pwd == NULL) exit ("getcwd error")//protection for if new_pwd returns NULL?
 	ms->pid = getpid();
 }
 
+//the minishell execution loop
 //starts readline loop
 //starts signals loop
 void	ft_minishell(t_ms *ms)
@@ -34,16 +35,17 @@ void	ft_minishell(t_ms *ms)
 	line = NULL;
 	while (2)
 	{
+		//start signals -	Kat
 		line = ft_readline();
 		if (ft_strlen(line) > 0)
 		{
 			ft_check_quotes((const char *)line);
 			ft_tokenize(line, ms);
 			ft_parse(ms);
+			// ft_exeggutor();
 		}
 		free (line);
 	}
-	//start signals -	Kat
 }
 
 //inits ms and env structs and stores envp
@@ -56,6 +58,8 @@ int	main(int argc, char **argv, char **envp)
 		return (EXIT_FAILURE);
 	ft_init_ms(&ms);
 	ms.env = start_env(envp);
+	if (ms.env == NULL)
+		return (EXIT_FAILURE);
 	ft_minishell(&ms);
 	return (0);
 }
