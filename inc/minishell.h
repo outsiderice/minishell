27bcd146c7c477f522c584467d6d95f8a9479f35
outsiderice <arantxamagnelli@gmail.com>
2024-07-10 12:09:58 +0200

    added 0 expansion

--- inc/minishell.h
+++ inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: amagnell <amagnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 12:40:16 by amagnell          #+#    #+#             */
-/*   Updated: 2024/07/09 17:39:11 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/07/10 12:02:23 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -131,6 +131,8 @@ int		ft_quote_len(const char *line, char type);
 /*    expand_utils.c   */
 char	*rm_delimiters(char *tok, int i);
 char	*all_join(char *s1, char *s2, char *s3);
+t_env	*find_env_var(t_env *env, char *var_name);
+char	*get_var_name(char *str, int start);
 // int		find_dollar_end(const char *name);
 
 /*    expand.c    */
