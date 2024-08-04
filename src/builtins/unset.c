69d22fc54a5cb5f1826d12f26e167e959abad61b
jjjjj <tdxgfhcgjvhkjxdgfhgcj@dfgfhgjh.com>
2024-08-04 19:45:10 +0200

    unset leaks fixed

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -6,7 +6,7 @@
 /*   By: kkoval <kkoval@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 15:58:31 by kkoval            #+#    #+#             */
-/*   Updated: 2024/07/29 15:30:40 by kkoval           ###   ########.fr       */
+/*   Updated: 2024/08/04 19:43:25 by kkoval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,9 @@ int ft_unset_args(t_env **env, char **args, int i)
                 prev->next = current->next;
             else
                 *env = current->next;
+            free(current->v_name);
+            free(current->v_cont);
+            free(current);
         }
         *env = first;
         i++;
