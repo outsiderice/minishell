d8cbe8f9675fe4a62ff8a3208b3c083266c19ad5
Kate <108870914+LeelooDa11as@users.noreply.github.com>
2024-08-06 16:37:46 +0200

    new export

--- inc/minishell.h
+++ inc/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkoval <kkoval@student.42.fr>              +#+  +:+       +#+        */
+/*   By: kate <kate@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 12:40:16 by amagnell          #+#    #+#             */
-/*   Updated: 2024/08/05 20:00:37 by kkoval           ###   ########.fr       */
+/*   Updated: 2024/08/06 16:07:22 by kate             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -216,6 +216,7 @@ int		*ft_sort_alpha(char **env, int len);
 
 /*    export_utils.c                           */
 void    ft_print_env(t_env *env_list, int fd);
+int		is_var_in_list(t_env *env_list, char *name);
 
 /*---------------------------------------------*/
 /*                   FREE                      */
