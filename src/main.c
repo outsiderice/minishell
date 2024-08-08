92bcff001fafc53039d31657e73a4c729b3b3d0d
outsiderice <arantxamagnelli@gmail.com>
2024-08-08 14:45:54 +0200

    norminette and revieweing

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: amagnell <amagnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 12:29:38 by amagnell          #+#    #+#             */
-/*   Updated: 2024/08/08 12:46:12 by amagnell         ###   ########.fr       */
+/*   Updated: 2024/08/08 14:32:16 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void	ft_init_ms(t_ms *ms, char **envp)
 {
-	char buffer[1024];
+	char	buffer[1024];
 
 	ms->env = start_env(envp);
 	if (ms->env == NULL || ft_set_shll_lvl(ms->env) == -1)
 	{
 		free_env(&ms->env);
-		return (exit (error_msg("env memory allocation failure\n", NULL)));
+		exit (error_msg("env memory allocation failure\n", NULL));
 	}
 	ms->tokens = NULL;
 	ms->args = NULL;
@@ -28,7 +28,6 @@ void	ft_init_ms(t_ms *ms, char **envp)
 	ms->sh_lvl = ft_get_shll_lvl(ms->env);
 	ms->pwd = getcwd(buffer, 1024);
 	ms->old_pwd = getcwd(buffer, 1024);
-	//printf("buffer: %s\n", buffer);
 	if (ms->old_pwd == NULL || ms->pwd == NULL)
 	{
 		free_env(&ms->env);
@@ -39,7 +38,7 @@ void	ft_init_ms(t_ms *ms, char **envp)
 
 //the minishell execution loop
 //starts readline loop
-//starts signals loop
+//starts signals
 void	ft_minishell(t_ms *ms)
 {
 	char	*line;
