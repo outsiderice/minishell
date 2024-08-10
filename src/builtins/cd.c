feb5907543f30d7cc7bb5bd22432b7de39f68c5c
jjjjj <tdxgfhcgjvhkjxdgfhgcj@dfgfhgjh.com>
2024-08-10 14:40:37 +0200

    cd:home path fix

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kate <kate@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: kkoval <kkoval@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 17:02:52 by kkoval            #+#    #+#             */
-/*   Updated: 2024/08/10 01:25:00 by kate             ###   ########.fr       */
+/*   Updated: 2024/08/10 14:39:01 by kkoval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	ft_change_pwd(t_ms *ms)
 	char	buffer[1024];
 
 	aux = ms->pwd;
-	ms->pwd = getcwd(buffer, 0);
+	ms->pwd = getcwd(buffer, 1024);
 	if (ms->pwd == NULL)
 		return (-1);
 	ms->old_pwd = aux;
@@ -59,18 +59,20 @@ int	ft_get_old_path(t_ms *ms, int fd)
 char	*ft_from_abs_path(t_ms *ms, char *arg)
 {
 	char	*path;
+	char	*aux;
 
 	path = get_env_cont(ms->env, "HOME");
 	if (!path)
 	{
-		printf("bash: cd: HOME NOT SET");
+		error_msg("cd: HOME NOT SET", NULL);
 		return (NULL);
 	}
 	if (arg == NULL || arg[1] == '\0')
 		return (path);
 	arg++;
-	path = ft_strjoin(path, arg);
-	return (path);
+	aux = ft_strjoin(path, arg);
+	free(path);
+	return (aux);
 }
 
 int	ft_cd(t_ms *ms, char **args, int fd)
@@ -79,15 +81,14 @@ int	ft_cd(t_ms *ms, char **args, int fd)
 	struct stat	sb;
 
 	path = NULL;
-	/*if (ft_args_len(args) > 2)
-	{
-		error_msg("eggshell: cd: too many arguments", NULL);
-		return (1);
-	}*/
 	if (ft_str_compare(args[1], "-") == 0)
 		return (ft_get_old_path(ms, fd));
 	else if (!args[1] || args[1][0] == '~')
+	{
 		path = ft_from_abs_path(ms, args[1]);
+		if (path == NULL)
+			return (1);
+	}
 	else
 		path = ft_strdup(args[1]);
 	if (chdir(path) == -1)
@@ -102,7 +103,6 @@ int	ft_cd(t_ms *ms, char **args, int fd)
 		return (1);
 	}
 	ft_change_pwd(ms);
-	if (path != NULL)
-		free(path);
+	free_char_ptr(path);
 	return (0);
 }
