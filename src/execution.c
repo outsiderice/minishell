4566029ab94154de9e7f81b08a98b94b618b4b32
outsiderice <arantxamagnelli@gmail.com>
2024-08-10 13:10:21 +0200

    about to reorganize executor

--- src/execution.c
+++ src/execution.c
@@ -3,23 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   execution.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkoval <kkoval@student.42.fr>              +#+  +:+       +#+        */
+/*   By: amagnell <amagnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 16:26:40 by amagnell          #+#    #+#             */
-/*   Updated: 2024/08/09 19:58:40 by kkoval           ###   ########.fr       */
+/*   Updated: 2024/08/10 13:09:29 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-//checks if it's a builtin and if it isn't calls execve
+
+
 void	exeggutor(t_ms *ms)
 {
 	ms->heredoc = handle_heredocs(ms);
 	if (ft_prep_args(ms) == 0 && ms->args->argv != NULL)
 	{
-		ft_exec(ms, ms->args);
-			//ms->exitstatus = 1;
+		if (ft_exec(ms, ms->args) == 1)
+			ms->exitstatus = 1;
 	}
 	free_tok_and_args(&ms->tokens, &ms->args);
 }
