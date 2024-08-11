98338c04256921528887ecfde9696888039819c0
jjjjj <tdxgfhcgjvhkjxdgfhgcj@dfgfhgjh.com>
2024-08-11 17:48:23 +0200

    norminette exec_path

--- src/exec_path.c
+++ src/exec_path.c
@@ -3,44 +3,45 @@
 /*                                                        :::      ::::::::   */
 /*   exec_path.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kate <kate@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: kkoval <kkoval@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 12:40:52 by kate              #+#    #+#             */
-/*   Updated: 2024/08/11 00:26:52 by kate             ###   ########.fr       */
+/*   Updated: 2024/08/11 17:47:32 by kkoval           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-char **ft_get_paths(t_env *env)
+char	**ft_get_paths(t_env *env)
 {
-    char	**paths;
-    while(env != NULL && ft_str_compare(env->v_name, "PATH") == 1)
-        env = env->next;
-    if (env == NULL)
+	char	**paths;
+
+	while (env != NULL && ft_str_compare(env->v_name, "PATH") == 1)
+		env = env->next;
+	if (env == NULL)
 		return (NULL);
 	paths = ft_split(env->v_cont, ':');
 	if (!paths)
 		return (NULL);
-    return (paths);
+	return (paths);
 }
 
 int	is_file_in_dir(char *file, char *dir)
 {
-	DIR 			*dirp;
-    struct dirent	*entry;
+	DIR				*dirp;
+	struct dirent	*entry;
 
-    dirp = opendir(dir);
-    if (dirp == NULL)
+	dirp = opendir(dir);
+	if (dirp == NULL)
 		return (1);
 	entry = readdir(dirp);
-    while (entry != NULL && ft_str_compare(file, entry->d_name) == 1)
+	while (entry != NULL && ft_str_compare(file, entry->d_name) == 1)
 		entry = readdir(dirp);
-    if (closedir(dirp) != 0)
+	if (closedir(dirp) != 0)
 		return (1);
 	if (entry == NULL)
-	    return (1);
-    return (0);
+		return (1);
+	return (0);
 }
 
 char	*ft_join_path(char *path, char *cmd)
@@ -51,34 +52,34 @@ char	*ft_join_path(char *path, char *cmd)
 	aux = ft_strjoin(path, "/");
 	res = ft_strjoin(aux, cmd);
 	free(aux);
-	return (res); 
+	return (res);
 }
 
-char *ft_find_path(char *file, char **paths)
+char	*ft_find_path(char *file, char **paths)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if (paths == NULL)
-        return (NULL); 
-    while (paths[i] != NULL && is_file_in_dir(file, paths[i]))
-        i++;
-    return (paths[i]);
+	i = 0;
+	if (paths == NULL)
+		return (NULL);
+	while (paths[i] != NULL && is_file_in_dir(file, paths[i]))
+		i++;
+	return (paths[i]);
 }
 
-int check_access(t_ms *ms, char *file)
+int	check_access(t_ms *ms, char *file)
 {
-    if (access(file, F_OK) == -1)
-    {
-        error_msg(file, " No such file or directory");
-        ms->exitstatus = 1;
-        return (1);
-    }
-    else if (access(file, W_OK) == -1)
-    {
-        error_msg(file, " Permisson denied");
-        ms->exitstatus = 1;
-        return (1);
-    }
-    return (0);
+	if (access(file, F_OK) == -1)
+	{
+		error_msg(file, " No such file or directory");
+		ms->exitstatus = 1;
+		return (1);
+	}
+	else if (access(file, W_OK) == -1)
+	{
+		error_msg(file, " Permisson denied");
+		ms->exitstatus = 1;
+		return (1);
+	}
+	return (0);
 }
