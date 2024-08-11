79ff38658163b662fac2aea443c78d4a5b1771fb
outsiderice <arantxamagnelli@gmail.com>
2024-08-11 18:01:33 +0200

    ft_init ms less lines

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kkoval <kkoval@student.42.fr>              +#+  +:+       +#+        */
+/*   By: amagnell <amagnell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/08 12:29:38 by amagnell          #+#    #+#             */
-/*   Updated: 2024/08/10 17:08:05 by kkoval           ###   ########.fr       */
+/*   Updated: 2024/08/11 18:00:28 by amagnell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,24 +23,18 @@ void	ft_init_ms(t_ms *ms, char **envp)
 		free_env(&ms->env);
 		exit (error_msg("env memory allocation failure", NULL));
 	}
-
 	ms->tokens = NULL;
 	ms->args = NULL;
 	ms->exitstatus = 0;
 	ms->sh_lvl = ft_get_shll_lvl(ms->env);
-	ms->old_pwd = NULL;
-	ms->pwd = NULL;
 	ms->old_pwd = ft_strdup(getcwd(buff_old, 1024));
-	if (ms->old_pwd == NULL)
+	ms->pwd = ft_strdup(getcwd(buff_pwd, 1024));
+	if (ms->old_pwd == NULL || ms->pwd == NULL)
 	{
 		free_env(&ms->env);
+		if (ms->old_pwd != NULL)
+			free(ms->old_pwd);
 		exit (error_msg("getcwd:Returned NULL old_pwd", NULL));
-	}
-	ms->pwd = ft_strdup(getcwd(buff_pwd, 1024));//limpiar memoria
-	if (ms->pwd == NULL)
-	{
-		free_env(&ms->env);
-	 	exit (error_msg("getcwd:Returned NULL new_pwd\n", NULL));
 	}
 	ms->pid = NULL;
 }
